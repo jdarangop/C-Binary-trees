9efd3712cbc5d42c973deccaaac23f313f20f40d
SantiagoHerreG <888@holbertonschool.com>
2019-12-03 22:38:41 +0000

    15 fixed null case

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,11 +9,13 @@
 int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (!tree)
-		return (1);
-	if (!binary_tree_is_full(tree->left))
 		return (0);
-	if (!binary_tree_is_full(tree->right))
-		return (0);
+	if (tree->left)
+		if (!binary_tree_is_full(tree->left))
+			return (0);
+	if (tree->right)
+		if (!binary_tree_is_full(tree->right))
+			return (0);
 	if ((tree->left && !tree->right) || (!tree->left && tree->right))
 		return (0);
 	return (1);
