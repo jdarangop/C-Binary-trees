309100c852a0f960448e66af0a7f008252778057
jdarangop <juandiegoap33@gmail.com>
2019-12-03 20:33:39 +0000

    Feature: add task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -12,12 +12,19 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t counter = 0;
+	const binary_tree_t *tmp;
+
 
 	if (tree == NULL)
 		return (0);
-	if (tree->left)
-		counter++;
-	if (tree->right)
-		counter++;
+
+	tmp = tree;
+	/*if (tmp->left)*/
+		counter += binary_tree_leaves(tmp->left);
+	/*if (tmp->right)*/
+		counter += binary_tree_leaves(tmp->right);
+	/*counter++;*/
+	if (tmp->left == NULL && tmp->right == NULL)
+		return (counter + 1);
 	return (counter);
 }
